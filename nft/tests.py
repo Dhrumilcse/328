24a2f0b40353f3ac14efd51af214613e22a43db5
Dhrumil Patel <dhrumilcse14@gmail.com>
2021-04-27 11:52:10 -0400

    Added HomePage Tests

--- nft/tests.py
+++ nft/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+from .models import Nft
 
 # Create your tests here.
+class HomePageTest(TestCase):
+
+    def setUp(self):
+        Nft.objects.create(title='just a test', price="45.9", image="media/csgo.png")
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_title_content(self):
+        nft = Nft.objects.get(id=1)
+        expected_object_name = f'{nft.title}'
+        self.assertEquals(expected_object_name, 'just a test')
+
+    def test_nft_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'just a test')
+        self.assertTemplateUsed(response, 'home.html')
