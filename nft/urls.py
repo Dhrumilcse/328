f81fa8271fb22961f39033cb4f5d083bbd081a6c
Dhrumil Patel <dhrumilcse14@gmail.com>
2021-05-04 11:26:50 -0400

    Fix: Login Required for Upload

--- nft/urls.py
+++ nft/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, re_path
 
 from .views import HomePageView, UploadNftView, SearchResultsView
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('upload/', UploadNftView.as_view(), name='upload'),
+    path('upload/', login_required(UploadNftView.as_view()), name='upload'),
     path('search/', SearchResultsView.as_view(), name='search_results'),
     path('profile/', views.profilePage , name='profile'),
     path('<id>/delete', views.deleteNftView, name='delete' ),
